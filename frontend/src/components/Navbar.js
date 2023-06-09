5635cd66593dd1de4d9019c35a3e98eebf6dd4d3
Mansoor Syed <mansoorhu10@gmail.com>
2023-06-08 20:39:00 -0400

    Fixed spelling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Grolisty</h1>
+                    <h1>GroListy</h1>
                 </Link>
             </div>
         </header>
