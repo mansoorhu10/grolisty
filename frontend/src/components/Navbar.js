79310f0d91aa5663e0373338cf3cc8d31e3f327b
Mansoor Syed <mansoorhu10@gmail.com>
2023-07-02 21:21:52 -0400

    Added initial authentication check for frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
 
     const handleClick = () => {
         logout();
@@ -15,13 +17,18 @@ const Navbar = () => {
                     <h1>GroListy</h1>
                 </Link>
                 <nav>
-                    <div>
-                        <button onClick={handleClick}>Log out</button>
-                    </div>
-                    <div>
-                        <Link to="/login">Log in</Link>
-                        <Link to="/signup">Sign Up</Link>
-                    </div>
+                    {user && (
+                        <div>
+                            <span>{user.email.substring(0, user.email.indexOf('@'))}</span>
+                            <button onClick={handleClick}>Log out</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to="/login">Log in</Link>
+                            <Link to="/signup">Sign Up</Link>
+                        </div>
+                    )}
                 </nav>
             </div>
         </header>
